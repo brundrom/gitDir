fab02cb7b2b28d700f831cdc3a9e1464869ca908
Gennadii Petrov <brundrom@gmail.com>
2023-01-20 21:16:02 +0300

    Added script for show two affirmations

---
+++ getAffirmation.js
@@ -0,0 +1,11 @@
+var number = Math.random()
+
+const yourAffirmation = function (number) {
+    if (number != 0) {
+        document.write("I am fo much stronger than I think")
+    } else {
+        document.write("It's okay to take time to myself")
+    }
+}
+
+yourAffirmation(number)
