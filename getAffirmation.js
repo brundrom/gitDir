2c4e4d89474f00204bbfbcf4b62ae1a4f00ff316
Gennadii Petrov <brundrom@gmail.com>
2023-01-22 20:08:25 +0300

    Try to fix

--- getAffirmation.js
+++ getAffirmation.js
@@ -1,7 +1,7 @@
 var number = Math.random()
 
 const yourAffirmation = function (number) {
-    if (number != 0) {
+    if (number != 1) {
         document.write("It's okay to take time to myself")
     } else {
         document.write("I am fo much stronger than I think")
