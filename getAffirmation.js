87d0a42c2d034d5156637f879e06abe7acc26b5a
Gennadii Petrov <brundrom@gmail.com>
2023-01-23 19:54:57 +0300

    Added new affirmation

--- getAffirmation.js
+++ getAffirmation.js
@@ -3,8 +3,10 @@ var number = Math.random()
 const yourAffirmation = function (number) {
     if (number != 1) {
         document.write("It's okay to take time to myself")
-    } else {
+    } if (number == 0) {
         document.write("I am fo much stronger than I think")
+    } else {
+        document.write("I let go of things that I have no control over")
     }
 }
 
