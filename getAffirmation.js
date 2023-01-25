c5b82085101eaf26b3a0a9a1d16859017e3d74e3
Gennadii Petrov <brundrom@gmail.com>
2023-01-25 23:04:23 +0300

    Bad commit

--- getAffirmation.js
+++ getAffirmation.js
@@ -2,8 +2,8 @@ var number = Math.random()
 
 const yourAffirmation = function (number) {
     if (number != 1) {
-        document.write("It's okay to take time to myself")
-    } if (number == 0) {
+        document.write("It's okay to take time to myself") //bad code/ this part not worked.
+    } if (number != 0) {
         document.write("I am fo much stronger than I think")
     } else {
         document.write("I let go of things that I have no control over")
